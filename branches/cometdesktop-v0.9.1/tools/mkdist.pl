50db8b7693b51dc78b8a673441999d917523ec04
david.davis <david.davis@12ea08fa-c998-11dd-a2ab-b939405fd53f>
2008-12-22 23:20:15 +0000

    release v0.9.1

---
+++ branches/cometdesktop-v0.9.1/tools/mkdist.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+open ( my $fh, "cometdesktop/VERSION" ) or die "VERSION: $!";
+
+my ( $v ) = <$fh>;
+chomp $v;
+
+my $file = "cometdesktop-v${v}.zip";
+
+system("rm $file") if ( -e $file );
+
+system('zip -r '.$file.' cometdesktop INSTALL LICENSE TODO README -i@tools/include.list -x@tools/exclude.list');
+
+print "\n\ncreated $file\n";
